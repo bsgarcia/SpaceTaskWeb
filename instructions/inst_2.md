99209ab1e32c0fc64b15a5f211f194433c540697
Basile Garcia <Basile.Garcia@unige.ch>
2023-12-19 17:54:25 +0100

    update

--- instructions/inst_2.md
+++ instructions/inst_2.md
@@ -1,15 +1,16 @@
 # Your objective 
-In this task, to maximize you overall score (displayed at the top left of the screen), there are two things you have to learn. 
-* **Spaceships' average rewards:** Learn the average reward of each spaceship. Each spaceship has a different average reward. 
-* **Forcefield resistance:** Estimate the forcefield resistance of the spaceships indicated by color. Forcefields are randomly drawn at the beginning of each trial. 
+In this task, to maximize you overall score (displayed at the top left of the screen), there are two things you have to learn. <br>
+🌌 **Forcefield resistance:** Estimate the forcefield resistance of the spaceships indicated by color. Forcefields are randomly drawn at the beginning of each trial. 
+<br>
+🚀**Spaceships' average rewards:** Learn the average reward of each spaceship. Each spaceship has a different average reward. 
 
 You will face waves of spaceship pairs. 
 In order to destroy a target, you have to press either left or right arrow key, and then press space to shoot, as demonstrated in the video on the right.  
 
 <!---
 admonition=<div class="admonition warning">
-	<p class="title">Warning!</p>
-	<p class="content">You can only shoot one option!</p>
+	<p class="title">Warning</p>
+	<p class="content">You can only shoot one spaceship per wave of 2!</p>
 </div>
 <br>
 <div class="admonition notice">
--- js/main.mjs
+++ js/main.mjs
@@ -3,6 +3,9 @@ import {getURLParams, createCode} from "./modules/utils.mjs";
 
 var instNum = 0;
 var maxInstNum = 3;
+var clickBlocked = false;
+const clickBlockedTime = 300;
+
 
 function main() {
     setSubID();
@@ -15,28 +18,52 @@ function main() {
     
 }
 
+const setStep = (step) => {
+    document.querySelector('#'+step).classList.add('active');
+    const stepList = ['introduction', 'experiment', 'survey'];
+    stepList.forEach(s => {
+        if (s != step) {
+            document.querySelector('#'+s).classList.remove('active');
+        }
+    })
+}
+
 const startGame = () => {
     // hide instructions
     document.querySelector('#panel').style.display = 'none';
-    
-    
-    
+    // set step
+    setStep('experiment');
+    document.querySelector('#game').style.display = 'block';
 }
 
 const setSubID = () => {
-    window.subID = getURLParams('prolificID');
+    window.subID = getURLParams('prolificID').toString();
     document.querySelector('.subID').innerHTML = 'id: ' + window.subID;
 }
 
 // function used to naviguate between instructions pages as markdown
 // using zero-md library
 const next = () => {
+    if (clickBlocked) return;
+    blockClick();
     instNum++;
     setPageInstruction(instNum);
 }
 const prev = () => {
+    if (clickBlocked) return;
+    blockClick();
     instNum--;
     setPageInstruction(instNum);
+}
+
+const hideButton = () => {
+    document.querySelector('#next-button').style.display = 'none';
+    document.querySelector('#prev-button').style.display = 'none';
+}
+
+const blockClick = () => {
+    clickBlocked = true;
+    setTimeout(() => {clickBlocked = false}, clickBlockedTime);
 }
 
 const setPageInstruction = async (instNum) => {
@@ -45,10 +72,17 @@ const setPageInstruction = async (instNum) => {
         document.querySelector('#prev-button').style.display = 'none';
     } else if (instNum > maxInstNum) {
         startGame();
+        hideButton();
     } else {    
         document.querySelector('#panel').innerHTML = await getInstructionPage(`./instructions/inst_${instNum}.md`)
         document.querySelector('#prev-button').style.display = 'inline-flex';
     }
+}
+
+window.gameEnded = () => {
+    setStep('survey');
+    // document.querySelector('#game').style.display = 'none';
+    // document.querySelector('#survey').style.display = 'block';
 }
 
 // ------------------------------ RUN ------------------------------ //
