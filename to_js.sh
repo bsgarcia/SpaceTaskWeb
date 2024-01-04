75d953a73ab4f5ab412c9ac137eb08641d9cf0e5
Basile Garcia <Basile.Garcia@unige.ch>
2024-01-04 17:08:27 +0100

    update

--- to_js.sh
+++ to_js.sh
@@ -1,5 +1,9 @@
 # bash script to rename .mjs to .js
 mv js/main.mjs js/main.js
+mv js/modules/*.mjs js/modules/*.js
+
+# rewrite imports in main.js
+sed -i 's/\.mjs/\.js/g' js/main.js
 
 # rewrite index.html to use .js instead of .mjs
 sed -i 's/main.mjs/main.js/g' index.html
